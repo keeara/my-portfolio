4adf7dd29c643bf4a0c39e986d566c73dad5392f
keeara <fran.chiaramonte0@gmail.com>
2024-10-19 19:12:57 +0200

    Adding home page details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Francesco <span className="text-[hsl(280,100%,70%)]">Chiaramonte</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -13,10 +13,10 @@ export default function HomePage() {
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <h3 className="text-2xl font-bold">Personal Information →</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              Phone Number: (+39) 327 615 88 31
+              Personal Email: fran.chiaramonte0@gmail.com
             </div>
           </Link>
           <Link
@@ -24,10 +24,9 @@ export default function HomePage() {
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <h3 className="text-2xl font-bold">My Curriculum →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+             <a href="https://drive.google.com/file/d/1SeBYWzrQ36qbmj94q5idKQPSbeU397dQ/view?usp=drive_link"> Show CV </a>
             </div>
           </Link>
         </div>
